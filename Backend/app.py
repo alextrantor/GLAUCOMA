261c9b82e956f9838a1676d3b70070f0ddac7fe5
alextrantor <german.p.g7@gmail.com>
2025-05-01 21:24:39 -0600

    ss

--- Backend/app.py
+++ Backend/app.py
@@ -1,43 +1,27 @@
-import requests
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 import io
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+
+# Cargar el modelo de validación de nervio óptico
+nervio_model = load_model('https://huggingface.co/Glaucomate/Modelo-glaucoma/blob/main/nervio_optico_modelo_mobilenet.h5', compile=False)
+
+# Cargar el modelo de clasificación de glaucoma
+glaucoma_model = load_model('https://huggingface.co/Glaucomate/Modelo-glaucoma/blob/main/modelo_clasificacion_glaucoma_mejorado.h5', compile=False)
 
 app = Flask(__name__)
-
-# Función para descargar el modelo desde Hugging Face
-def download_model(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Guardar el modelo en un archivo temporal en memoria
-        return tf.keras.models.load_model(io.BytesIO(response.content))
-    else:
-        raise Exception(f"Error al descargar el modelo: {response.status_code}")
-
-# Descargar los modelos de Hugging Face
-nervio_model_url = 'https://huggingface.co/Glaucomate/Modelo-glaucoma/blob/main/nervio_optico_modelo_mobilenet.h5'
-glaucoma_model_url = 'https://huggingface.co/Glaucomate/Modelo-glaucoma/blob/main/modelo_clasificacion_glaucoma_mejorado.h5'
-
-try:
-    nervio_model = download_model(nervio_model_url)
-    glaucoma_model = download_model(glaucoma_model_url)
-    print("Modelos cargados correctamente.")
-except Exception as e:
-    print(f"Error al cargar los modelos: {str(e)}")
 
 # Ruta para procesar las imágenes
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image part'}), 400
-    
+
     file = request.files['image']
     img = Image.open(file.stream).convert('RGB')
-    img = img.resize((224, 224))  # Redimensionar la imagen
+    img = img.resize((224, 224))  # Redimensionar la imagen para que coincida con la entrada del modelo
 
     # Convertir la imagen en array
     img_array = np.array(img) / 255.0  # Normalizar la imagen
@@ -59,4 +43,4 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
