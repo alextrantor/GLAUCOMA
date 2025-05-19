7b0de3e2363002d34e22f657784c99f96f9792ca
alextrantor <german.p.g7@gmail.com>
2025-05-18 19:44:51 -0600

    rr

--- Frontend/src/ResultsDisplay.jsx
+++ Frontend/src/ResultsDisplay.jsx
@@ -9,7 +9,7 @@ function ResultsDisplay({ results }) {
           <p>Nervio Óptico Detectado: {results.is_nerve ? 'Sí' : 'No'}</p>
           {results.is_nerve && (
             <div>
-              <p>CDR предicha: {results.cdr_prediction ? results.cdr_prediction.toFixed(4) : 'N/A'}</p>
+              <p>CDR: {results.cdr_prediction ? results.cdr_prediction.toFixed(4) : 'N/A'}</p>
               <p>Sospecha de Glaucoma: {results.glaucoma_suspected ? 'Sí' : 'No'}</p>
             </div>
           )}
